199de91fd7d38e185fa49deec73da928bbdabbf8
Koonchung <ElvinChan0644@outlook.com>
2018-10-16 12:55:03 +0800

    Update code.

--- newjwxt/pingjiao.py
+++ newjwxt/pingjiao.py
@@ -5,6 +5,9 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re
 from newjwxt import newjwxt
+import getpass
+import os
+
 
 url_base = 'http://newjwxt.bjfu.edu.cn'
 url_main = 'http://newjwxt.bjfu.edu.cn/jsxsd/xspj/xspj_find.do?Ves632DSdyV=NEW_XSD_JXPJ'
@@ -17,14 +20,15 @@ class JXPJ(object):
     def __init__(self, username, password):
         self.session = newjwxt(username, password).login()
 
+    # 获取学期评教url
     def get_url(self):
         r = self.session.get(url_main)
         soup = bs(r.content, 'html.parser')
-        # print(soup)
         s = soup.find_all("a", {"title": "点击进入评价"})
         url = url_base + s[0]['href']
         return url
 
+    # 获取待评教课程url
     def get_courses(self):
         url_target = self.get_url()
         r = self.session.get(url_target)
@@ -34,6 +38,7 @@ class JXPJ(object):
             target_urls.append(url_base + part.split('\'')[0])
         return target_urls
 
+    # 构造formdata并评教
     def evaluate(self):
         target_urls = self.get_courses()
         for url in target_urls:
@@ -60,14 +65,20 @@ class JXPJ(object):
                 data = soup.find_all("input", {"name": s+num})[1]['value']
                 formdata[s+num] = data
             self.session.post(url_post, formdata)
-        print("Completed.")
+        print("评教完成:)")
 
 
 def main():
-    username = input("请输入学号: ")
-    password = input("请输入教务系统密码: ")
-    print("评教中...")
-    JXPJ(username, password).evaluate()
+    state = True
+    while(state):
+        username = input("请输入学号: ")
+        password = getpass.getpass("请输入教务系统密码: ")
+        try:
+            JXPJ(username, password).evaluate()
+            state = False
+            os.system('pause')
+        except:
+            state = True
 
 
 if __name__ == '__main__':
